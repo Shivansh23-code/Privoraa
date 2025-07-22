20cda3c2d0410eb6cafd349dc11242533232e3c6
Shivansh Tiwari <shivanshtiwari887@gmail.com>
2025-07-22 10:58:45 +0530

    Added Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import LandingPage from './pages/LandingPage';
 import Footer from './components/Footer';
+import { Analytics } from '@vercel/analytics/react'; 
 import './index.css';
+
 
 function App() {
   return (
     <>
       <LandingPage />
       <Footer />
+      <Analytics />
     </>
   );
 }
